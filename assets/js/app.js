c2b6cb824f175478c13bb62378798a7579157b90
Sam <baleclyps@hotmail.fr>
2017-03-10 11:30:48 +0100

    modification

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,25 @@
 //REVISIONS
 // Declarez une variable nommée "boucler" contenant true
-
+var boucler = true;
 
 // Declarez un tableau members contenant Aida67, lapie002, anneserrano, Jennysmille, nunkabuk, RCosson, kaonb-ax, FerEmilie, crazychouwi, KiluaZoldyc, patatobeur, Sam11360, elo062, hermeline, Biciclet,
-
+var tableau = ['Aida67', 'lapie002', 'anneserrano', 'Jennysmille', 'nunkabuk', 'RCosson', 'kaonb-ax', 'FerEmilie', 'crazychouwi', 'KiluaZoldyc', 'patatobeur', 'Sam11360', 'elo062', 'hermeline', 'Biciclet'];
 // SI la variable boucler vaut true ALORS
+$("#check").click(function(){
+  if (boucler===true){
+    for (var i = 11; i < tableau.length; i++) {
+      console.log(tableau[i]);
+      switch (members) {
+        case Sam11360:
 
+          break;
+        default:
+
+      }
+    }
+  }
+});
+$("#resultatAjax").load("contenu.html");
   // Bouclez sur le tableau que vous avez déclaré ci-dessus
 
   // Mettre un switch pour qu'au moment où la boucle se trouve sur votre pseudo cela ajoute "Affiche " devant votre pseudo dans la console et sur l'écran et par defaut seulement le pseudo des autres
